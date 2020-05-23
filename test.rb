65853deb97a3d602f548630d2862637a431121f1
DAZUN XING <dazun.xing@gmail.com>
2020-05-23 22:38:19 +0000

    Done.

---
+++ test.rb
@@ -0,0 +1,18 @@
+require_relative './lib/person.rb'
+
+info={
+      :name => "Avi",
+      :birthday => "01/29/1984",
+      :hair_color => "brown",
+      :eye_color => "brown",
+      :height => "short",
+      :weight => "good",
+      :handed => "lefty",
+      :complexion => "decent",
+      :t_shirt_size => "medium",
+      :wrist_size => "small",
+      :glove_size => "normal",
+      :pant_length => "32",
+      :pant_width => "32"
+    }
+avi=Person.new(info)
